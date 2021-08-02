9ee9733d075ebab85495ec57ec92e8a824173621
E-crown <87323964+e-crowngroup@users.noreply.github.com>
2021-08-02 16:54:45 -0300

    teste-front-end-dashboard
    dashboard em teste

---
+++ precache-manifest.756a8e68e1c676ba177ba160eff28fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6fbc9df18c49e659dc84e2155723ed4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49e032778a46382f2ea9",
+    "url": "/static/css/main.dc716f11.chunk.css"
+  },
+  {
+    "revision": "f2828a45361a51284119",
+    "url": "/static/js/2.823f3e00.chunk.js"
+  },
+  {
+    "revision": "b76cd0b7294280e24e2077be230640d3",
+    "url": "/static/js/2.823f3e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e032778a46382f2ea9",
+    "url": "/static/js/main.5a5b78cc.chunk.js"
+  },
+  {
+    "revision": "c5f758ab953c6d6f2987",
+    "url": "/static/js/runtime-main.63661cca.js"
+  }
+]);
